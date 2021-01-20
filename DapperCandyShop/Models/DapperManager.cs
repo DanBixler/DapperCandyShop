44878f40a6c942de1b45820c5d0cddb4bf5f29b1
Daniel Bixler <danielbixler@yahoo.com>
2021-01-19 20:45:30 -0500

    Added some code and a couple of files to manage Dapper operations

---
+++ DapperCandyShop/Models/DapperManager.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperCandyShop.Models
+{
+    public class DapperManager : IDapperManager
+    {
+        private readonly IConfiguration _config;
+        public DapperManager(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public DbConnection GetConnection()
+        {
+            return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        }
+
+        public T Get<T>(string storedProc, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll<T>(string storedProc, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DapperCandyShop/Models/Interfaces/IDapperManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.Common;
+
+namespace DapperCandyShop.Models
+{
+    public interface IDapperManager : IDisposable
+    {
+        DbConnection GetConnection();
+        T Get<T>(string storedProc, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        List<T> GetAll<T>(string storedProc, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+    }
+}
